2df992b8ab9bf957d25a25f6b1421f5dad4d8337
saif-shayeb <sshayep@gmail.com>
2026-03-06 02:17:40 +0200

    first commit

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ def reset_database():
     return jsonify({"message": "Database reset successfully"}), 200
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",debug=True)
